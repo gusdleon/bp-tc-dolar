9fcba6eba01d7d94d2dcb8c65fbd55e5bde38b81
Gustavo de León <alfonso.gus.deleon@gmail.com>
2024-05-03 02:25:13 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,33 @@ import { getDateBP } from "./getDateBP";
 import { saveDolar } from "./saveDolar";
 import { respuestatc } from "./respuestatc";
 
+/**
+ * Módulo principal que exporta un objeto con dos funciones asincrónicas: fetch y scheduled.
+ * La función fetch se encarga de manejar las solicitudes GET a la ruta "/MX/tc_barmesa/_tipo-de-cambio.html".
+ * La función scheduled se ejecuta periódicamente y realiza la obtención y procesamiento de datos del dólar.
+ *
+ * @module index
+ */
+
 export default {
+	// #region función fetch (manejo de solicitudes GET)
 	/**
-	 * Realiza una solicitud para obtener datos XML de la URL proporcionada y devuelve una respuesta con un valor de dólar específico.
-	 * @param {Request} request - La solicitud de la función fetch.
-	 * @param {any} env - El entorno de ejecución.
-	 * @param {any} ctx - El contexto de ejecución.
-	 * @returns {Response} - La respuesta que contiene el valor del dólar en formato HTML.
+	 * Función asincrónica que maneja las solicitudes GET a la ruta "/MX/tc_barmesa/_tipo-de-cambio.html".
+	 * Realiza validaciones de la solicitud y obtiene el valor del dólar guardado en la caché.
+	 * Luego, devuelve una respuesta con el valor del dólar.
+	 *
+	 * @param {Request} request - La solicitud HTTP recibida.
+	 * @param {Object} env - El entorno de ejecución.
+	 * @param {Object} ctx - El contexto de ejecución.
+	 * @returns {Response} - La respuesta HTTP con el valor del dólar.
 	 */
 	async fetch(request, env, ctx) {
-		// Verificamos que la solicitud sea un método GET y que la URL sea la correcta
 		// #region Validaciones de la solicitud
+		/** Se verifica que el método de la solicitud sea GET
+		 * y que la ruta sea "/MX/tc_barmesa/_tipo-de-cambio.html".
+		 * Si no cumple con las validaciones, se devuelve una respuesta
+		 * con el código de estado correspondiente.
+		 */
 		const url = new URL(request.url);
 		if (request.method !== "GET") {
 			console.warn("Método no permitido");
@@ -23,13 +39,27 @@ export default {
 			return new Response("URL no permitida", { status: 403 });
 		}
 		// #endregion
-		// Obtenemos el precio mínimo del dólar de la base de datos de clave-valor de DOF (con un tiempo de vida en caché de 1 hora)
+
+		// Se obtiene el valor del dólar guardado en la caché
+		// y se devuelve una respuesta con el valor del dólar y la fecha
 		const kVFecha = await env.kvdof.get("fecha", { cacheTtl: 3600 });
-		//console.log(`Fecha guardada: ${kVFecha}`);
 		const kVPrecio = Number(await env.kvdof.get("precio", { cacheTtl: 3600 })).toFixed(4);
-		//console.log(`Precio guardado: ${kVPrecio}`);
 		return respuestatc(kVFecha, kVPrecio);
 	},
+	// #endregion
+
+	// #region función scheduled (obtención y procesamiento de datos del dólar)
+	/**
+	 * Función asincrónica que se ejecuta periódicamente y realiza la obtención y procesamiento de datos del dólar.
+	 * Obtiene el valor mínimo permitido del dólar guardado en la caché.
+	 * Luego, obtiene el XML del DOF y extrae el valor y la fecha del dólar.
+	 * Si el valor del dólar es menor al mínimo permitido, guarda el mínimo permitido y la fecha en la caché.
+	 * Si el valor del dólar es mayor o igual al mínimo permitido, guarda el valor y la fecha en la caché.
+	 *
+	 * @param {Event} event - El evento programado.
+	 * @param {Object} env - El entorno de ejecución.
+	 * @param {Object} ctx - El contexto de ejecución.
+	 */
 	async scheduled(event, env, ctx) {
 		const precioMinimoPermitido = Number(await env.kvdof.get("precioMinimo", { cacheTtl: 3600 })).toFixed(4);
 		console.log(`Precio minimo permitido: ${precioMinimoPermitido}`);
@@ -37,7 +67,7 @@ export default {
 		// Intentamos obtener los datos XML de la URL proporcionada
 		xmlText = await getxml();
 
-		//  Definimos las etiquetas de inicio y fin para extraer el valor del dólar
+		//  Definimos las etiquetas de inicio y fin para extraer el valor del dólar y la fecha
 		const itemEndTag = '</item>';
 		const descriptionStartTag = '<description>';
 		const descriptionEndTag = '</description>';
@@ -49,8 +79,8 @@ export default {
 		const endIndex = xmlText.indexOf(itemEndTag, startIndex);
 
 		if (startIndex === -1 || endIndex === -1) {
-			console.warn('No se encontró el valor del dólar en el XML del DOF, Enviando ultima fecha y precio guardado en cache');
-			return; // Terminate the execution without returning anything
+			console.warn('No se encontró el valor del dólar en el XML del DOF, Omitiendo... y finalizando la ejecución.');
+			return; // Si no se encuentra el valor del dólar, se finaliza la ejecución
 		}
 
 		// El precio del dólar se encuentra entre las etiquetas de descripción asi que lo extraemos y lo convertimos a un número
@@ -65,11 +95,12 @@ export default {
 		const valorDolar = Number(dolarDescription).toFixed(4);
 
 		if (valorDolar < precioMinimoPermitido) {
-			console.warn(`El valor del dólar ${valorDolar} es menor al precio mínimo permitido , Enviando fecha y precio minimo`);
+			console.warn(`El valor del dólar ${valorDolar} es menor al precio mínimo permitido, Enviando fecha y precio minimo`);
 			await saveDolar(env, precioMinimoPermitido, fechaDolar);
 			return;
 		}
 		console.log(`Guardando valor del dolar: ${valorDolar} con fecha: ${fechaDolar}`);
 		await saveDolar(env, valorDolar, fechaDolar);
 	},
+	// #endregion
 };
